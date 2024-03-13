ab152a2fb7374ff604b936a7e46a95e28ea826e2
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2024-03-13 04:12:11 +0330

    feat: add config.

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => '127.0.0.1',
+    'port' => '3370',
+    'dbname' => 'meeting',
+    'username' => 'root',
+    'password' => 'secret',
+    'charset' => 'utf8mb4',
+];
