b18a7dc05f121b0a08a62e0c1d6c0f8259b014bd
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2024-03-13 04:13:03 +0330

    feat: add service and repo.

---
+++ src/App/Repository/MeetingRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Meeting\App\Repository;
+
+use Meeting\App\Entity\Meeting;
+use Meeting\Core\Database;
+
+class MeetingRepository
+{
+    public function __construct(private Database $database)
+    {}
+
+    public function findForUser(int $userId, string $startDate, string $endData): array
+    {
+        $sql = "SELECT * FROM `meetings` WHERE user_id = :user_id and (date between :start and :end);";
+
+        $conn = $this->database->connection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([
+            'start' => $startDate,
+            'end' => $endData,
+            'user_id' => $userId,
+        ]);
+
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS|\PDO::FETCH_PROPS_LATE, Meeting::class);
+    }
+
+
+    function save(array $data)
+    {
+        /** @var Database $db */
+        $connection = $this->database->connection();
+
+        $connection->beginTransaction();
+
+        try {
+            $sql = "
+            INSERT INTO `meetings` (start_time, end_time, date, user_id)
+            VALUES (:start, :end, :date, :user_id)
+            ";
+
+
+            $this->database->query($sql, [
+                'start' => $data['start'],
+                'end' => $data['end'],
+                'date' => $data['date'],
+                'user_id' => $data['user_id'],
+            ]);
+
+
+            $connection->commit();
+        } catch (\Exception $e) {
+            $connection->rollBack();
+
+            throw $e;
+        }
+
+    }
+
+}
---
+++ src/App/Service/MeetingService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Meeting\App\Service;
+
+use Meeting\App\Repository\MeetingRepository;
+
+class MeetingService
+{
+    public function __construct(private MeetingRepository $meetingRepository)
+    {
+    }
+
+    public function findForUser(int $userId): array
+    {
+        $start = new \DateTimeImmutable('Asia/Tehran');
+        $end = $start->add(new \DateInterval('P7D'));
+
+
+        return $this->meetingRepository->findForUser(
+            $userId, $start->format('Y-m-d'), $end->format('Y-m-d')
+        );
+    }
+
+    public function save(array $data)
+    {
+        $this->meetingRepository->save($data);
+    }
+}
